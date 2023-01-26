061e95e0755fd514d4dee461cf736231f820581b
Fahad <fahadahammed1@gmail.com>
2023-01-26 22:48:43 +0600

    functionality

--- Module 26/js/banking.js
+++ Module 26/js/banking.js
@@ -1,16 +1,56 @@
 // console.log('js conected')
 
+function getInputValue(inputId)
+{
+    const inputFild = document.getElementById(inputId);
+    const AmountText = inputFild.value;
+    const amount = parseFloat(AmountText);
+    inputFild.value = '';
+
+    return amount;
+}
+
+
 document.getElementById('deposit-button').addEventListener('click', function(){
-    // console.log("clicked in deposit button");
-    const depositInput = document.getElementById('deposit-input');
-    const depositAmount = parseFloat(depositInput.value);
-    // console.log(depositAmount);
+    
+    const depositAmount = getInputValue('deposit-input');
     
     //get current deposit
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
+    const previousDepositTotal = parseFloat(depositTotalText);
+
+    depositTotal.innerText = depositAmount + previousDepositTotal;
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const totalBalanceText = balanceTotal.innerText;
+    const previousBalance = parseFloat(totalBalanceText);
+
+    balanceTotal.innerText = previousBalance + depositAmount;
     
 
-    depositInput.value = '';
+    // depositInput.value = '';
+});
+
+//withdraw
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    
+    const withdrawAmount = getInputValue('withdraw-input');
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalAmount = parseFloat(withdrawTotal.innerText);
+    
+
+    withdrawTotal.innerText = withdrawAmount + withdrawTotalAmount;
+
+    //update balance 
+    const balanceTotal = document.getElementById('balance-total');
+    const totalBalanceText = balanceTotal.innerText;
+    const previousBalance = parseFloat(totalBalanceText);
+
+    balanceTotal.innerText = previousBalance - withdrawAmount;
+
+    // withdrawInput.value = '';
 });
