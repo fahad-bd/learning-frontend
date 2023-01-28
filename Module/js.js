d8361453b3b710604035f7e49e7dfcca586fc77a
Fahad <fahadahammed1@gmail.com>
2023-01-29 00:41:53 +0600

    added

---
+++ Module/js.js
@@ -0,0 +1,15 @@
+let count = 0;
+console.log(parseInt("count"+1));
+
+const a = Math.pow(4,0.5)+"spiderman"+"987"+Math.sqrt(9);
+console.log(a);
+
+
+function generatePin()
+{
+    return Math.floor(Math.random()*90000)+10000;
+}
+console.log(generatePin());
+
+const aa = isNaN('10X');
+console.log(aa);
