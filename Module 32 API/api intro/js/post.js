70e52493475e5815b0eb479b29b2347b97ca57bb
Fahad <fahadahammed1@gmail.com>
2023-02-02 21:35:50 +0600

    put on server

---
+++ Module 32 API/api intro/js/post.js
@@ -0,0 +1,39 @@
+// console.log('js');
+function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPost(data));
+}
+loadPost();
+
+function displayPost(posts){
+    const postContainer = document.getElementById('posts');
+    for(const post of posts){
+        const div = document.createElement('div');
+        div.classList.add('post');
+        div.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        `;
+        postContainer.appendChild(div);
+        console.log(post);
+    }
+}
+
+
+function addPost()
+{
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new post',
+            body: 'This i smy posts',
+            userId: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
