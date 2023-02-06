934d643120fc2fe358c14bd7eeeadfe8308919ac
Fahad <fahadahammed1@gmail.com>
2023-02-06 20:39:14 +0600

    type of variable

---
+++ Module 34/data-type.js
@@ -0,0 +1,15 @@
+//premative
+let a = 19;
+let b = a ;
+console.log(a, b);
+
+a = 12;
+console.log(a, b);
+
+//non prematiove
+let x = {name: "fahad"};
+let y = x;
+console.log(x, y);
+
+x.name = "fahim";
+console.log(x, y);
