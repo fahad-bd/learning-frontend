05a3632d66670d37f075ee10b049ab792d596344
Fahad <fahadahammed1@gmail.com>
2023-02-06 21:22:22 +0600

    double equal vs triple equal

---
+++ equal.js
@@ -0,0 +1,11 @@
+//== or ===
+const a = 2;
+const b = '2';
+if(a==b) console.log('True');
+else console.log('False');
+
+
+const c = 2;
+const d = '2';
+if(c === d) console.log('True');
+else console.log('False');
